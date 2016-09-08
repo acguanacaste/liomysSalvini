abda361b1631eeec345313cad6e73133cec0dc74
marcosjv87 <marcjv182@gmail.com>
2016-09-08 16:43:05 -0600

    Creacion del modulo de Observacion

---
+++ server/controllers/observacion.controller.js
@@ -0,0 +1,5 @@
+import Observacion from '../models/observacion';
+
+export function getSomething(req, res) {
+  return res.status(200).end();
+}
---
+++ server/routes/observacion.routes.js
@@ -0,0 +1,7 @@
+import { Router } from 'express';
+import * as ObservacionController from '../controllers/observacion.controller';
+
+const router = new Router();
+
+
+export default router;

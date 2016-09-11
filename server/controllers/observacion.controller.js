fdd8651a6cb68db8d632971e86c4d1fef2fa5799
Federico Matarrita Arata <federico@usematics.com>
2016-09-11 17:38:27 -0600

    Agregar rutas de observaciones en server.js y crear acciones

--- server/controllers/observacion.controller.js
+++ server/controllers/observacion.controller.js
@@ -1,5 +1,17 @@
 import Observacion from '../models/observacion';
+import cuid from 'cuid';
+import slug from 'limax';
+import sanitizeHtml from 'sanitize-html';
+
 
 export function getSomething(req, res) {
   return res.status(200).end();
+}
+
+
+export function getObservacion(req, res) {
+    var posts = "";
+    console.log("Aqui van las observaciones");
+    res.json({ posts });
+
 }

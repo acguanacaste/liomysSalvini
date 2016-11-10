33d286caeed0ee54635e972b880d6d65c5d15edd
HernandezCarlos <carher88@gmail.com>
2016-11-10 14:39:09 -0600

    Agredadas funciones para el procesamiento de las taxonomias delcsv

--- server/sample.js
+++ server/sample.js
@@ -46,6 +46,26 @@
           return arrayExisting === row.family;
         }
 
+        function findOrder(arrayExisting){
+          return arrayExisting === row.order;
+        }
+
+        function findPhylum(arrayExisting){
+          return arrayExisting === row.phylum;
+        }
+
+        function findGenus(arrayExisting){
+          return arrayExisting === row.genus;
+        }
+
+        function findKingdom(arrayExisting){
+          return arrayExisting === row.kingdom;
+        }
+
+        function findClass(arrayExisting){
+          return arrayExisting === row.class;
+        }
+
         function findscientificName(arrayExistingScientificName){
           return arrayExistingScientificName === row.scientificname;
         }
@@ -65,14 +85,37 @@
           });
         }
 
+        if (arrayExisting.find(findOrder) === undefined) { //family is not in existing array
+          taxonomies.insert({taxonomyType : "order", taxonomyName : row.order.trim(), description : "example description"});
+          arrayExisting.push(row.order);
+        }
+
+        if (arrayExisting.find(findPhylum) === undefined) { //family is not in existing array
+          taxonomies.insert({taxonomyType : "phylum", taxonomyName : row.phylum.trim(), description : "example description"});
+          arrayExisting.push(row.phylum);
+        }
+
+        if (arrayExisting.find(findGenus) === undefined) { //family is not in existing array
+          taxonomies.insert({taxonomyType : "genus", taxonomyName : row.genus.trim(), description : "example description"});
+          arrayExisting.push(row.genus);
+        }
+
+        if (arrayExisting.find(findKingdom) === undefined) { //family is not in existing array
+          taxonomies.insert({taxonomyType : "kingdom", taxonomyName : row.kingdom.trim(), description : "example description"});
+          arrayExisting.push(row.kingdom);
+        }
+
+       if (arrayExisting.find(findClass) === undefined) { //family is not in existing array
+          taxonomies.insert({taxonomyType : "class", taxonomyName : row.class.trim(), description : "example description"});
+          arrayExisting.push(row.class);
+        }
+
         if (arrayExistingScientificName.find(findscientificName) === undefined ) {
           if (jsonTaxonomies.find(findIDTaxonomy) !== undefined) {
             species.insert({ idPadre: jsonTaxonomies.find(findIDTaxonomy)._id,scientificName : row.scientificname, description : "example description"});
             arrayExistingScientificName.push(row.scientificname);
           }
         }
-
-
   		});//push
   	})//transform
   	.on('error', function (err) {
@@ -82,8 +125,8 @@
   		queue.drain = function() {
   			sightings.count(function(err, count) {
   				console.log('Number of documents:', count);
-          console.log(arrayExisting);
-          console.log(jsonTaxonomies);
+          /*console.log(arrayExisting);
+          console.log(jsonTaxonomies);*/
   				db.close();
   			});
   		};

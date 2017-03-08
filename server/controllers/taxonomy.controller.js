bd7eaa559e62f754d457938378d444df63ce195e
jmoragarios <jmoragarios@gmail.com>
2017-03-08 03:09:57 -0600

    Se agregaron rutas al modulod de taxonomias

--- server/controllers/taxonomy.controller.js
+++ server/controllers/taxonomy.controller.js
@@ -7,21 +7,37 @@ export function getSomething(req, res) {
 export function getTaxonomys(req, res) {
 
     Taxonomy.find().exec((err, responseResult) => {
-      if (err) {
+      if (err || responseResult === null) {
         res.status(500).send(err);
+      }else {
+        var responseHeader = {totalresults : Object.keys(responseResult).length, start : 0 , end : Object.keys(responseResult).length};
+        res.json({ responseHeader, responseResult });
       }
-      var responseHeader = {totalresults : Object.keys(responseResult).length, start : 0 , end : Object.keys(responseResult).length};
-      res.json({ responseHeader, responseResult });
+
     });
 }//getSTaxonomys
 
 //Get one Taxonomy by taxonomyName
 export function getTaxonomy(req, res) {
   Taxonomy.findOne({ taxonomyName : req.params.request }).exec((err, responseResult) => {
-    if (err) {
+    if (err || responseResult === null) {
       res.status(500).send(err);
+    }else {
+      var responseHeader = { totalresults : Object.keys(responseResult).length, start : 0 , end : Object.keys(responseResult).length};
+      res.json({ responseHeader, responseResult });
     }
-    var responseHeader = { totalresults : Object.keys(responseResult).length, start : 0 , end : Object.keys(responseResult).length};
-    res.json({ responseHeader, responseResult });
+
   });
 }//getTaxonomy
+
+// Get Taxonomies by type
+export function getTaxonomybyType(req, res){
+  Taxonomy.find({taxonomyType : req.params.taxonomyType}).exec((err, responseResult) => {
+    if(err || responseResult === null){
+      res.status(500).send(err);
+    }else {
+      var responseHeader = {totalresults : Object.keys(responseResult).length, start : 0 , end : Object.keys(responseResult).length};
+      res.json({responseHeader,responseResult});
+    }
+  });
+}// getTaxonomybyType
--- server/routes/taxonomy.routes.js
+++ server/routes/taxonomy.routes.js
@@ -3,11 +3,14 @@ import * as TaxonomyController from '../controllers/taxonomy.controller';
 
 const router = new Router();
 
-//Get all taxonomies
+// Get all taxonomies
 router.route('/Taxonomias').get(TaxonomyController.getTaxonomys);
 
-//Get one Taxonomy by taxonomyname
+// Get one Taxonomy by taxonomyname
 router.route('/Taxonomias/:request').get(TaxonomyController.getTaxonomy);
+
+// Get Taxonomies by type
+router.route('/Taxonomias/tipoTaxonomia/:taxonomyType').get(TaxonomyController.getTaxonomybyType);
 
 
 export default router;

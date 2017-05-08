6370125b105fdfe21adc4d83fc75bbfc7449cedf
jmoragarios <jmoragarios@gmail.com>
2017-05-08 16:38:14 -0600

    Se agrego paquete crypto y nuevos routes para sightings
    paquete crypto de npm para la funcionalidad de token y state encriptados, y las nuevas rutas por necesidad del app movil.

--- server/controllers/sighting.controller.js
+++ server/controllers/sighting.controller.js
@@ -2,6 +2,7 @@ import Sighting from '../models/sighting';
 import cuid from 'cuid';
 import slug from 'limax';
 import sanitizeHtml from 'sanitize-html';
+import crypto from 'crypto';
 
 
 export function getSomething(req, res) {
@@ -13,15 +14,23 @@ export function getSightings(req, res) {
     //var posts = "";
     //console.log("Aqui van las observaciones");
     Sighting.find().exec((err, responseResult) => {
-      if (err) {
+      if (err || responseResult === null ) {
         res.status(500).send(err);
+      }else{
+        var responseHeader = {totalresults : Object.keys(responseResult).length, start : 0 , end : Object.keys(responseResult).length};
+        res.json({ responseHeader, responseResult });
       }
-      var responseHeader = {totalresults : Object.keys(responseResult).length, start : 0 , end : Object.keys(responseResult).length};
-      res.json({ responseHeader, responseResult });
     });
     //res.json({ posts });
 }
 
+// Get all sightings with token
+/*export function getSightingsbyToken(req, res){
+  let encrypToken = req.params.token;
+  let appName = req.params.appName;
+  let decrypToken = crypto.createHmac('sha1', appName).update(text).digest('hex');
+}
+*/
 // Get one sighting by catalogNumber
 export function getSighting(req, res) {
   Sighting.findOne({ catalogNumber : req.params.request }).exec((err, responseResult) => {
@@ -39,6 +48,34 @@ export function getSightingsSpecie(req,res){
 
   Sighting.find(request).exec((err,responseResult) => {
     if (err || responseResult === null) {
+      res.status(500).send(err);
+    }else {
+      const responseHeader = { totalresults : Object.keys(responseResult).length, start : 0 , end : Object.keys(responseResult).length};
+      res.json({responseHeader, responseResult});
+    }
+  });
+}
+
+//Get sightings by Genus
+export function getSightingsGenus(req,res){
+  const request = { genus : req.params.genus }
+
+  Sighting.find(request).exec((err, responseResult) => {
+    if(err || responseResult === null){
+      res.status(500).send(err);
+    }else{
+      const responseHeader = { totalresults : Object.keys(responseResult).length, start : 0 , end : Object.keys(responseResult).length};
+      res.json({responseHeader, responseResult});
+    }
+  });
+}
+
+//Get sightings by family
+export function getSightingsFamily(req, res){
+  const request = { family : req.params.family }
+
+  Sighting.find(request).exec((err,responseResult)=>{
+    if(err || responseResult === null ){
       res.status(500).send(err);
     }else {
       const responseHeader = { totalresults : Object.keys(responseResult).length, start : 0 , end : Object.keys(responseResult).length};
--- server/routes/sighting.routes.js
+++ server/routes/sighting.routes.js
@@ -11,4 +11,13 @@ router.route('/Observaciones/:request').get(SightingController.getSighting);
 // Get sightings by specie
 router.route('/Observaciones/especie/:specie').get(SightingController.getSightingsSpecie);
 
+//GEt sighting by genus
+router.route('/Observaciones/genero/:genus').get(SightingController.getSightingsGenus);
+
+//Get sightings by family
+router.route('/Observaciones/familia/:family').get(SightingController.getSightingsFamily);
+
+// Get sightings with token
+//router.route('/:token/:appName/Observaciones').get(SightingController.getSightingsbyToken);
+
 export default router;
